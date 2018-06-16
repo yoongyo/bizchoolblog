125f54f0fdf38ca1288b1b1bb865689507f8e6d0
yoongyo <jyg017@naver.com>
2018-06-17 08:31:07 +1000

    ㅡ

--- ch1/mysite/settings/common.py
+++ ch1/mysite/settings/common.py
@@ -18,7 +18,24 @@ from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from os.path import abspath, dirname, join
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') # secrets.json 파일 위치를 명시
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # Users/Jarvis/desktop/myblog/ch1/settings/common.py
 BASE_DIR = dirname(dirname(dirname(abspath(__file__))))
