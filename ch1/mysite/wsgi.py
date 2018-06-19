33dc954c8643716f2ef31b3009216380ccaf62da
yoongyo <jyg017@naver.com>
2018-06-20 02:34:07 +1000

    g2

--- ch1/mysite/wsgi.py
+++ ch1/mysite/wsgi.py
@@ -5,7 +5,7 @@ path = '/home/bizchool/bizhcool.pythonanywhere.com'
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 applcation = get_wsgi_application()
